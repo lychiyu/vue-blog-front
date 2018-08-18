897b0b06e7c56a383036425f43f986dcc0d7a061
liuying <liuyingover@163.com>
2018-08-18 10:37:17 +0800

    init file strcture

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,9 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'components': resolve('src/components'),
+      'views': resolve('src/views'),
+      'styles': resolve('src/assets/styles'),
     }
   },
   module: {
