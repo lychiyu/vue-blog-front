109899b6fbc5d47d330804c1d22f90ea76d2021b
liuying <liuyingover@163.com>
2018-08-19 02:32:23 +0800

    finish categroy and tags pc style

---
+++ src/components/CateTitle.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="title">
+      <p>{{name}} : {{title}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CateTitle',
+  data () {
+    return {
+      name: 'Category',
+      title: 'Tutorials'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .title p
+    font-family "Microsoft Yahei"
+    padding 20px 0 30px
+    &::before
+      content "#"
+      color #eb5055
+      padding-right 10px
+</style>
---
+++ src/views/categroy/Categroy.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="categroy">
+      <blog-nav></blog-nav>
+      <cate-list></cate-list>
+      <copyright></copyright>
+    </div>
+</template>
+
+<script>
+import BlogNav from 'components/Nav'
+import CateList from './components/CateList'
+import Copyright from 'components/Copyright'
+
+export default {
+  name: 'Categroy',
+  components: {
+    BlogNav,
+    CateList,
+    Copyright
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
